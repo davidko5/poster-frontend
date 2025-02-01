86fa8c503c9d507522f0fc9e540a698beb8a2472
Davyd Kondratenko <dajan526@gmail.com>
2025-02-01 12:54:34 +0100

    feat: add spinner is posts/users are loading
    - remove user select is users are not loaded yet
    - add spinner is posts/users are loading

--- src/features/posts/PostList.tsx
+++ src/features/posts/PostList.tsx
@@ -1,21 +1,22 @@
 import { useState, useRef } from "react"
 import { useAppSelector, useAppDispatch } from "../../app/hooks"
-import { selectPostsIds, addPost } from "./postsSlice"
+import { addPost, selectPostIdsOrUndefined } from "./postsSlice"
 import styles from "./Posts.module.scss"
 import { TextareaModal } from "./TextareaModal"
 import { PostExcerpt } from "./PostExcerpt"
+import { RiseLoader } from "react-spinners"
 
 const frontendBaseUrl = import.meta.env.VITE_BASE_URL
 
 export const PostList = () => {
   const dispatch = useAppDispatch()
 
-  const orderedPostsIds = useAppSelector(selectPostsIds)
+  const orderedPostsIds = useAppSelector(selectPostIdsOrUndefined)
   const currentUser = useAppSelector((state) => state.users.currentUser)
 
   const [addPostModalOpen, setAddPostModalOpen] = useState(false)
   const addPostModalContainerRef = useRef(null)
-  const layedPosts = orderedPostsIds.map((postId) => {
+  const layedPosts = orderedPostsIds?.map((postId) => {
     return (
       <PostExcerpt key={postId} postId={postId} currentUser={currentUser} />
     )
@@ -23,7 +24,21 @@ export const PostList = () => {
 
   return (
     <div className={styles.postListPageContainer}>
-      {orderedPostsIds.length ? (
+      {!orderedPostsIds ? (
+        <div
+          data-testid="noPostsModal"
+          style={{ flexDirection: "column" }}
+          className={styles.notFoundContainer}
+        >
+          <p>
+            Loading content. It can take up to a minute since free server is
+            used
+          </p>
+          <div style={{ marginBottom: "16px" }}>
+            <RiseLoader color="hsl(212, 24%, 26%)" />
+          </div>
+        </div>
+      ) : orderedPostsIds.length ? (
         <div className={styles.postsPreviewContainer}>{layedPosts}</div>
       ) : (
         <div data-testid="noPostsModal" className={styles.notFoundContainer}>
